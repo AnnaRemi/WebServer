dabdafc106ef35e4388c0b00e061ba3ece498b42
AnnaRemi <60964233+AnnaRemi@users.noreply.github.com>
2020-05-17 12:10:16 +0300

    Update my_server.py
    pep-8

--- my_server.py
+++ my_server.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template, request
 from PyDictionary_library import get_meaning, get_translation
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route("/", methods=["GET", "POST"])
 def get_home():
     if request.method == "POST":
-	input = request.form["word"]
-	MESSAGE = '''
-    		<html>
-		    <body>
-			<p>{result}</p>
-			<p><a href="/">Click here to go on</a>
-	    	    </body>
-		</html>
+        input = request.form["word"]
+        MESSAGE = '''
+            <html>
+            <body>
+            <p>{result}</p>
+            <p><a href="/">Click here to go on</a>
+            </body>
+            </html>
                '''
-	if input != None:
-	    if request.form["submit_button"] == "Find the definition": 
-	        result = get_meaning(input)
-	        return MESSAGE.format(result=result)
+        if input is not None:
+            if request.form["submit_button"] == "Find the definition":
+                result = get_meaning(input)
+                return MESSAGE.format(result=result)
 
-	    elif request.form["submit_button"] == "Find the translation":
-		code = request.form["code"]
-		result = get_translation(code, input)
-		return MESSAGE.format(result=result)
+            elif request.form["submit_button"] == "Find the translation":
+                code = request.form["code"]
+                result = get_translation(code, input)
+                return MESSAGE.format(result=result)
 
     return render_template("home.html")
 
