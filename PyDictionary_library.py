b077792c55960bf0127b18a261026c00e72f5b10
AnnaRemi <remizova.annn@gmail.com>
2020-05-17 12:06:57 +0300

    checked pep-8

--- PyDictionary_library.py
+++ PyDictionary_library.py
@@ -3,24 +3,24 @@ import json
 import requests
 from PyDictionary import PyDictionary
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/api/meaning/<name>',methods=['GET'])
+@app.route('/api/meaning/<name>', methods=['GET'])
 def get_meaning(name):
-	dictionary=PyDictionary()
-        value=dictionary.meaning(name)
-        if value is None:
-                return json.dumps({'error':'Word has no meaning in API'})
-        return json.dumps(value)
+    dictionary = PyDictionary()
+    value = dictionary.meaning(name)
+    if value is None:
+        return json.dumps({'error': 'Word has no meaning in API'})
+    return json.dumps(value)
+
 
 @app.route('/api/translate/<code>/<word>', methods=['GET'])
 def get_translation(word, code):
-	dictionary=PyDictionary()
-        value=dictionary.translate(word, code)
-        if value is None:
-                return json.dumps({'error':'Word has no translation in API'})
-        return json.dumps(value)
-
+    dictionary = PyDictionary()
+    value = dictionary.translate(word, code)
+    if value is None:
+        return json.dumps({'error': 'Word has no translation in API'})
+    return json.dumps(value)
 
