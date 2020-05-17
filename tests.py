e994ed6a6cbed90a05c59b00761c03c47809ef5f
AnnaRemi <60964233+AnnaRemi@users.noreply.github.com>
2020-05-17 12:20:35 +0300

    checked pep-8

--- tests.py
+++ tests.py
@@ -1,24 +1,31 @@
 import unittest
 from PyDictionary import PyDictionary
 
+
 class API_Tester(unittest.TestCase):
+    ok = 200
+
     def setUp(self):
         self.dictionary = PyDictionary
+
     def test_meaninig(self):
-        self.assertEqual(self.dictionary.meaning("play").status_code, 200)
+        self.assertEqual(self.dictionary.meaning("play").status_code, ok)
+
     def test_translate(self):
-        self.assertEqual(self.dictionary.translate("play", "cy").status_code, 200)
+        self.assertEqual(self.dictionary.translate("go", "cy").status_code, ok)
+
     def test_translate(self):
-        self.assertEqual(self.dictionary.translate("play", "es").status_code, 200)
+        self.assertEqual(self.dictionary.translate("go", "es").status_code, ok)
+
     def test_translate(self):
-        self.assertEqual(self.dictionary.translate("play", "de").status_code, 200)
+        self.assertEqual(self.dictionary.translate("go", "de").status_code, ok)
+
     def test_translate(self):
-        self.assertEqual(self.dictionary.translate("play", "it").status_code, 200)
+        self.assertEqual(self.dictionary.translate("go", "it").status_code, ok)
+
     def test_translate(self):
-        self.assertEqual(self.dictionary.translate("play", "hi").status_code, 200)
+        self.assertEqual(self.dictionary.translate("go", "hi").status_code, ok)
 
 
 if __name__ == '__main__':
     unittest.main()
-        
-        
