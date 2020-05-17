f484ea066064e57e2408daaae558ab2103ec67d1
AnnaRemi <remizova.annn@gmail.com>
2020-05-17 23:58:59 +0300

    huuuuge fixes in translation, output and appearence

--- PyDictionary_library.py
+++ PyDictionary_library.py
@@ -2,6 +2,10 @@ from flask import Flask, request
 import json
 import requests
 from PyDictionary import PyDictionary
+from translate import Translator
+import pprint 
+from pprint import pformat
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,14 +17,15 @@ def get_meaning(name):
     value = dictionary.meaning(name)
     if value is None:
         return json.dumps({'error': 'Word has no meaning in API'})
-    return json.dumps(value)
+    return value
 
 
 @app.route('/api/translate/<code>/<word>', methods=['GET'])
 def get_translation(word, code):
-    dictionary = PyDictionary()
-    value = dictionary.translate(word, code)
+    lang = str(code)
+    translator = Translator(to_lang = lang)
+    value = translator.translate(str(word))
     if value is None:
         return json.dumps({'error': 'Word has no translation in API'})
-    return json.dumps(value)
+    return json.dumps(value, ensure_ascii=False).encode('utf8')
 
--- readme.md
+++ readme.md
@@ -24,8 +24,6 @@ There you'll see such window:
 this is `home` page
 
 on `about` page you can learn about me and my project
-on `home` page you can insert a word in special window; afterwards, you can either choose one of five languages ans translate your word, or you can ger its definition by clicking the appropriate button  
+on `home` page you can insert a word in special window; afterwards, you can either choose one of six languages ans translate your word, or you can ger its definition by clicking the appropriate button  
 
-for some words' translations there is an output `{"error": "Word has no translation in API"}`
-it happens due to change in Google translate's policy, as PyDictionary uses Google translate
-https://stackoverflow.com/questions/37009972/pydictionary-translate-returns-invalid-word-none
+
