6415cce0bdb87e53d22e370f36aa9801aa3b0e37
AnnaRemi <remizova.annn@gmail.com>
2020-05-18 11:48:29 +0300

    .

--- tests.py
+++ tests.py
@@ -11,7 +11,6 @@ class API_Tester(unittest.TestCase):
     def test_meaninig(self):
         self.assertEqual(self.dictionary.meaning("play").status_code, ok)
 
-<<<<<<< HEAD
     def test_translate(self):
         self.assertEqual(self.dictionary.translate("go", "fr").status_code, ok)
 
@@ -29,28 +28,9 @@ class API_Tester(unittest.TestCase):
 
     def test_translate(self):
         self.assertEqual(self.dictionary.translate("go", "es").status_code, ok)
-=======
-    def test_translate(self):
-        self.assertEqual(self.dictionary.translate("go", "cy").status_code, ok)
-
-    def test_translate(self):
-        self.assertEqual(self.dictionary.translate("go", "es").status_code, ok)
-
-    def test_translate(self):
-        self.assertEqual(self.dictionary.translate("go", "de").status_code, ok)
-
-    def test_translate(self):
-        self.assertEqual(self.dictionary.translate("go", "it").status_code, ok)
-
-    def test_translate(self):
-        self.assertEqual(self.dictionary.translate("go", "hi").status_code, ok)
->>>>>>> e994ed6a6cbed90a05c59b00761c03c47809ef5f
 
 
 if __name__ == '__main__':
     unittest.main()
-<<<<<<< HEAD
 
 
-=======
->>>>>>> e994ed6a6cbed90a05c59b00761c03c47809ef5f
